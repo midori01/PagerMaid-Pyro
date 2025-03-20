5072c68aaf179cce2732a61ae06c239ea06c986b
ミドリ <midori01@users.noreply.github.com>
2025-03-20 10:34:32 +0900

    Update update.py

--- pagermaid/common/update.py
+++ pagermaid/common/update.py
@@ -7,6 +7,6 @@ async def update(force: bool = False):
         await execute("git reset --hard origin/master")
     else:
         await execute("git diff --quiet || git stash")
-    await execute("git pull")
+    await execute("git pull --all")
     await execute(f"{executable} -m pip install --upgrade -r requirements.txt --break-system-packages")
     await execute("git stash pop || true")
