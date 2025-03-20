e62e905c88d384c7cca57c9e8bb21ce5aa6b1943
ミドリ <midori01@users.noreply.github.com>
2025-03-20 09:51:19 +0900

    Update update.py

--- pagermaid/common/update.py
+++ pagermaid/common/update.py
@@ -1,13 +1,12 @@
 from sys import executable
-
 from pagermaid.utils import execute
-
 
 async def update(force: bool = False):
     await execute("git fetch --all")
     if force:
         await execute("git reset --hard origin/master")
-    await execute("git pull --all")
-    await execute(f"{executable} -m pip install --upgrade -r requirements.txt")
-    await execute(f"{executable} -m pip install -r requirements.txt")
-    await execute(f"{executable} -m pip install -r requirements.txt --break-system-packages")
+    else:
+        await execute("git diff --quiet || git stash")
+    await execute("git pull")
+    await execute(f"{executable} -m pip install --upgrade -r requirements.txt --break-system-packages")
+    await execute("git stash pop || true")
